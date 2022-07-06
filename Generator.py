21f0ee67fc27316fe3bc30f7f5451c9d64ab4485
TasciBerkan <berkan1695@gmail.com>
2022-07-06 17:55:32 +0300

    First Commit

---
+++ Generator.py
@@ -0,0 +1,54 @@
+import os
+import random
+from datetime import datetime
+# pip install Faker
+from faker import Faker
+from time import sleep
+from faker.providers import BaseProvider
+import logging
+# pip install pyzmq
+import zmq
+
+random.seed = 0
+Faker.seed = 0
+
+logging.basicConfig()
+_logger = logging.getLogger('test')
+_logger.setLevel(logging.DEBUG)
+
+
+class ModuleProvider(BaseProvider):
+    def module_name(self):
+        return random.choice(['evse', 'ocpp', 'slac'])
+
+    def module_state(self):
+        # Random states are not really reasonable, but this is just for testing.
+        return random.choice(['initializing', 'idle', 'charging', 'finalizing'])
+
+    def module_output(self):
+        # Make stdout have 2/3 probability.
+        stream = random.choice(['stdout', 'stdout', 'stderr'])
+        message = 'Something happened.'
+        return (stream, message)
+
+
+fake = Faker()
+fake.add_provider(ModuleProvider)
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5555")
+
+while True:
+    log_output = fake.module_output()
+    msg = {
+        'module': fake.module_name(),
+        'timestamp': datetime.now().isoformat(),
+        'state': fake.module_state(),
+        'log-stream': log_output[0],
+        'log-message': log_output[1],
+    }
+    _logger.debug(msg)
+    socket.send_json(msg)
+
+    sleep(random.uniform(0, 0.5))
